eb02d69ab6027f71865839d4d1f618432726a873
Roya <foroughan.royaa@gmail.com>
2023-11-28 17:30:06 +0330

    first config

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config()
+
+async function main(){
+    const app = express()
+    const port = process.env.PORT
+    require('./src/config/mongoose.config')
+    app.listen(4000 , () => {
+        console.log(`server: http://localhost:${port}`);
+    })
+}
+
+main()
---
+++ src/config/mongoose.config.js
@@ -0,0 +1,11 @@
+const {default : mongoose} = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+    console.log('Connect to db');
+}).catch(err => {
+    console.log(err?.message ?? 'Failed db connection');
+})
+
